b41405721d85b2aaf0c09ff1691b31224469c0ab
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2021-10-24 20:58:22 +0700

    Login/Singup/Viewdata page done

--- beranda/urls.py
+++ beranda/urls.py
@@ -1,6 +1,13 @@
 from django.urls import include, path
-from .views import index
+from .views import dataPage, index, json_data, logoutUser, dataPage
+from .views import loginPage
+from .views import daftarPage
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login', loginPage, name='login'),
+    path('daftar', daftarPage, name='daftar'),
+    path('logout', logoutUser, name='logout'),
+    path('data', dataPage, name='data'),
+    path('json',json_data, name='json'),
 ]
--- vaksinfo/urls.py
+++ vaksinfo/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 import beranda.urls as beranda
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(beranda)),
