348c48c2988397c19ba6a9e059906e4e145ffd39
ryan-sujana <ryansujanahusein@gmail.com>
2021-10-27 12:38:23 +0700

    Fixed ordering and removed unnecessary codes

--- artikel/views.py
+++ artikel/views.py
@@ -9,29 +9,16 @@ def index(request):
 class ArtikelListView(ListView):
     model = Artikel
     template_name = 'artikel_list.html'
-    ordering = ['-id']
 
     def get_queryset(self):
         query = self.request.GET.get('q')
+        
         if query:
-            object_list = self.model.objects.filter(Q(body__icontains=query) | Q(title__icontains=query))
+            object_list = self.model.objects.filter(Q(body__icontains=query) | Q(title__icontains=query)).order_by('-id')
         else:
-            object_list = self.model.objects.all()
+            object_list = self.model.objects.all().order_by('-id')
         return object_list
 
 class ArtikelDetailView(DetailView):
     model = Artikel
     template_name = 'artikel_detail.html'
-
-def get_article_queryset(query=None):
-    queryset = []
-    queries = query.split(" ")
-    for q in queries:
-        articles = Artikel.objects.filter(
-            Q(title__icontains=q) |
-            Q(body__icontains = q)
-        ).distinct()
-
-        for article in articles:
-            queryset.append(article);
-    return list(set(queryset))
