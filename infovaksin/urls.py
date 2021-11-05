dd096430c795869b2edf0d824e91b6126ea9b281
Adrian Hakim Utomo <adrianhakimutomo@gmail.com>
2021-11-05 21:10:54 +0700

    added popup and AJAX for info vaksin

--- infovaksin/urls.py
+++ infovaksin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import*
 
+
 urlpatterns = [
-    path('', index, name='index_infovaksin')
+    path('', index, name='index_infovaksin'),
+    path('show_json', vaksindata, name='show_json')
 ]
