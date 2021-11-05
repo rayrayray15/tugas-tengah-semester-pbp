6ae6f8ab0b7513d22377908943abb2b23bce254a
alma.aqila@ui.ac.id <alma.aqila@ui.ac.id>
2021-11-05 12:03:33 +0700

    implements search function

--- faq/views.py
+++ faq/views.py
@@ -8,6 +8,12 @@ def index(request):
     page = Paginator(faq, 6)
     page_number = request.GET.get('page')
     page_obj = page.get_page(page_number)
-	
+
+    if request.method == "POST":
+        query_name = request.POST.get('question', None)
+        if query_name:
+            results = Faq.objects.filter(question__icontains=query_name)
+            return render(request, 'index_faq.html', {"results":results})
+    
     return render(request, 'index_faq.html', {'faq':page_obj})
     
